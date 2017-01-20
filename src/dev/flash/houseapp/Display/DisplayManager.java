55903443aefe2cd4ececf86035a94d97e45b84a7
Flash <flash.jaq@gmail.com>
2017-01-20 01:08:21 +0000

    Created opengl display

---
+++ src/dev/flash/houseapp/Display/DisplayManager.java
@@ -0,0 +1,57 @@
+package dev.flash.houseapp.Display;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.Sys;
+import org.lwjgl.opengl.*;
+
+/**
+ * Created by Flash on 20/01/2017.
+ */
+
+public class DisplayManager {
+
+    public static int WIDTH = 1280;
+    public static int HEIGHT = 720;
+    private static int FPS_CAP = 240;
+
+
+    private static long lastFrameTime;
+    private static float delta;
+
+    public static void createDisplay() {
+        ContextAttribs attribs = new ContextAttribs(3, 2).withForwardCompatible(true).withProfileCore(true);
+
+        try {
+            Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+            Display.create(new PixelFormat(), attribs);
+            Display.setTitle("House App");
+
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+        }
+        GL11.glViewport(0, 0, WIDTH, HEIGHT);
+        lastFrameTime = getCurrentTime();
+    }
+
+
+    public static void updateDisplay() {
+        Display.sync(FPS_CAP);
+        Display.update();
+        long currentFrameTime = getCurrentTime();
+        delta = (currentFrameTime - lastFrameTime);
+        lastFrameTime = currentFrameTime;
+    }
+
+    public static float getFrameTimeMillis() {
+        return delta;
+    }
+
+    public static void closeDisplay() {
+        Display.destroy();
+    }
+
+    private static long getCurrentTime() {
+        return Sys.getTime() * 1000 / Sys.getTimerResolution();
+    }
+
+}
--- src/dev/flash/houseapp/Launcher.java
+++ src/dev/flash/houseapp/Launcher.java
@@ -1,11 +1,28 @@
 package dev.flash.houseapp;
 
+import dev.flash.houseapp.Display.DisplayManager;
+import org.lwjgl.opengl.Display;
+
 /**
  * Created by Flash on 19/01/2017.
  */
 
 public class Launcher {
     public static void main(String[] args) {
-        return;
+        DisplayManager.createDisplay();
+        while (! Display.isCloseRequested()) {
+            Display.setTitle(Float.toString(1000 / DisplayManager.getFrameTimeMillis()));
+
+            ////UPDATE////
+
+
+            ////RENDER////
+
+
+            //Push to Screen
+            DisplayManager.updateDisplay();
+        }
+
+        DisplayManager.closeDisplay();
     }
 }
